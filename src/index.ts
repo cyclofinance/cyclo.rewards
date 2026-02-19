eccd77c00309edb994991f9c6a85899cabe4f800
rouzwelt <rouzweltt@gmail.com>
2026-02-19 22:40:07 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,14 +33,14 @@ async function main() {
 
   // Read transfers file
   console.log("Reading transfers file...");
-  const transfers = []
+  let transfers: any[] = []
   for (let i = 0; i < 10; i++) {
     const transfersData = await readFile(`data/transfers${i + 1}.dat`, "utf8").catch(() => "");
     const transfersBatch = transfersData
       .split("\n")
       .filter(Boolean)
       .map((line) => JSON.parse(line));
-    transfers.push(...transfersBatch)
+    transfers = [...transfers, ...transfersBatch]
   }
   console.log(`Found ${transfers.length} transfers`);
 
